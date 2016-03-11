6fea9677b440030241784f59ea301ba791b2ce85
Sarah Stevens <sstevens2@wisc.edu>
2016-03-11 13:42:42 -0600

    Added functionailty for if there are zero hits and columns for ref and meta

--- scripts/calcCovFromDepth.py
+++ scripts/calcCovFromDepth.py
@@ -12,16 +12,30 @@ if len(sys.argv) != 2:
 	usage()
 	exit()
 
-# Read in depth file
+# Get argument
 filename=sys.argv[1]
-depth_df= pd.read_table(filename, header=None)
-covCol = len(depth_df.columns)-1 #Get last column number
+covFileHeader='filename\ref\meta\tbaseCovSum\tAvgCov\n'
+regCovLine='{}\t{}\t{}\t{}\t{}\n'
+ref,_,meta=os.path.basename(filename).split('-')
+meta=os.path.splitext(meta)[0]
 
-# Makes a coverage file if none exists or writes line to existing one.
-if os.path.exists('coverage.txt'):
-	with open('coverage.txt','a') as outfile:
-		outfile.write('{}\t{}\t{}\n'.format(filename,depth_df[covCol].sum(), depth_df[covCol].mean()))
-else:
-	with open('coverage.txt','a') as outfile:
-		outfile.write('filename\tbaseCovSum\tAvgCov\n')
-		outfile.write('{}\t{}\t{}\n'.format(filename,depth_df[covCol].sum(), depth_df[covCol].mean()))
+if os.stat(filename).st_size == 0: #Checking to see if file is empty (which would happen if no reads recruited)
+	if os.path.exists('coverage.txt'):
+		# Makes a coverage file if none exists or writes line to existing one.
+		with open('coverage.txt','a') as outfile:
+			outfile.write(regCovLine.format(filename,ref,meta,'0', '0')) #Records coverage 0
+	else:
+		with open('coverage.txt','a') as outfile:
+			outfile.write(covFileHeader)
+			outfile.write(regCovLine.format(filename,ref,meta,'0', '0'))
+else: #if file is not empty
+	depth_df= pd.read_table(filename, header=None) #Read in file
+	covCol = len(depth_df.columns)-1 #Get last column number
+	# Makes a coverage file if none exists or writes line to existing one.
+	if os.path.exists('coverage.txt'):
+		with open('coverage.txt','a') as outfile:
+			outfile.write(regCovLine.format(filename,ref, meta, depth_df[covCol].sum(), depth_df[covCol].mean()))
+	else:
+		with open('coverage.txt','a') as outfile:
+			outfile.write(covFileHeader)
+			outfile.write(regCovLine.format(filename,ref, meta, depth_df[covCol].sum(), depth_df[covCol].mean()))
