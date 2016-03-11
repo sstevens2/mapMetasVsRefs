dd4fc86ab485168d45307ce1851238f6fe52ec86
Sarah Stevens <sstevens2@wisc.edu>
2016-03-11 13:18:45 -0600

    Added opt for bbmap to be in alt locations

--- scripts/runMapping.py
+++ scripts/runMapping.py
@@ -7,21 +7,23 @@ __author__ = "Sarah Stevens"
 __email__ = "sstevens2@wisc.edu"
 
 def usage():
-	print "Usage: runmapping.py combolist threads(int) memlimitPerjob"
+	print "Usage: runmapping.py combolist bbpath threads(int) memlimitPerjob"
 
-if len(sys.argv) != 4:
+if len(sys.argv) != 5:
 	usage()
 	exit()
 
 with open(sys.argv[1],'r') as f:
 	combolist = f.read().splitlines()
-threads=int(sys.argv[2])
-memlimit=sys.argv[3]
+bbpath=sys.argv[2]
+threads=int(sys.argv[3])
+memlimit=sys.argv[4]
+
 
 def run_mapping(params):
 	reffile, metafile, outfile = params.split('\t')
 	if not os.path.exists(outfile):
-		cmd = ["../bbmap/bbmap.sh", "ref="+reffile,"in="+metafile,"outm="+outfile,"idtag","minid=.8","threads=1","bandwidthratio=.8","nodisk","-Xmx"+memlimit]
+		cmd = [bbpath, "ref="+reffile,"in="+metafile,"outm="+outfile,"idtag","minid=.8","threads=1","nodisk","-Xmx"+memlimit]
 		#print ' '.join(cmd), os.getpid()
 		subprocess.call(cmd)
 	else:
