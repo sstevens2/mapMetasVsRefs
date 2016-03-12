be54aecce9d30fefa28aa753baf562bb226ca81c
Sarah Stevens <sstevens2@wisc.edu>
2016-03-12 11:22:22 -0600

    Add ANI files to reset script

--- resetFiles.sh
+++ resetFiles.sh
@@ -7,7 +7,9 @@ if [ "$1" = "True" ]
 then
 	rm mappingResults/*.bam
 	rm mappingResults/*.depth
+        rm mappingResilts/*.pid*
 fi
 rm resultingPIDs.txt
 rm parsedPID.txt
 rm coverage.txt
+rm ani.txt
