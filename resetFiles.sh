623af85dcd52dfcdba755ece93cd29618bb2e5f0
Sarah Stevens <sstevens2@wisc.edu>
2016-03-12 20:24:02 -0600

    Added refGenome length file to ignore files

--- resetFiles.sh
+++ resetFiles.sh
@@ -5,11 +5,10 @@ rm metagenomeList.txt
 rm mappingCombos.txt
 if [ "$1" = "True" ]
 then
-	rm mappingResults/*.bam
-	rm mappingResults/*.depth
-        rm mappingResilts/*.pid*
+	rm mappingResults/*
 fi
 rm resultingPIDs.txt
 rm parsedPID.txt
 rm coverage.txt
 rm ani.txt
+rm refGenomes.len
